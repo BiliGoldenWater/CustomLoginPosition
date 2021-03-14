e0cff5a0877859dcdf1d443b512e0d2551ba915c
BiliGoldenWater <2439577268@qq.com>
2021-03-14 22:21:02 +0800

    fix version in help message

--- src/main/java/io/github/biligoldenwater/customloginposition/commands/CommandCustomLoginPosition.java
+++ src/main/java/io/github/biligoldenwater/customloginposition/commands/CommandCustomLoginPosition.java
@@ -15,7 +15,8 @@ import java.util.List;
 public class CommandCustomLoginPosition {
     JavaPlugin plugin;
     Server server;
-    public CommandCustomLoginPosition(JavaPlugin plugin){
+
+    public CommandCustomLoginPosition(JavaPlugin plugin) {
         this.plugin = plugin;
         this.server = plugin.getServer();
         this.plugin.getCommand("customloginposition").setExecutor(customloginposition_command);
@@ -24,15 +25,15 @@ public class CommandCustomLoginPosition {
     CommandExecutor customloginposition_command = new CommandExecutor() {
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            switch (args.length){
+            switch (args.length) {
                 case 1:
-                    switch (args[0]){
+                    switch (args[0]) {
                         case "help":
-                            if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.help")){
+                            if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.help")) {
                                 sender.sendMessage("§cYou don't have permission to use this command.");
                                 return true;
                             }
-                            sender.sendMessage("CustomLoginPosition 1.0-SNAPSHOT by.Golden_Water");
+                            sender.sendMessage("CustomLoginPosition 1.0-beta by.Golden_Water");
                             sender.sendMessage("Usage:/clp help (Show this message)");
                             sender.sendMessage("Usage:/clp setloginposition (Set your position to login position)");
                             sender.sendMessage("Usage:/clp banworld <add|remove> (Add or remove your world to banworld list)");
@@ -43,32 +44,32 @@ public class CommandCustomLoginPosition {
                                 sender.sendMessage("§cOnly player can use this command!");
                                 return true;
                             }
-                            if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.setloginposition")){
+                            if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.setloginposition")) {
                                 sender.sendMessage("§cYou don't have permission to use this command.");
                                 return true;
                             }
                             Player player = (Player) sender;
                             Configuration config = plugin.getConfig();
                             Location location = player.getLocation();
-                            double playerX=location.getX(),
-                                    playerY=location.getY(),
-                                    playerZ=location.getZ();
-                            float playerYaw=location.getYaw(),
-                                    playerPitch=location.getPitch();
-                            config.set("loginWorld",player.getWorld().getName());
-                            config.set("loginPositionX",playerX);
-                            config.set("loginPositionY",playerY);
-                            config.set("loginPositionZ",playerZ);
-                            config.set("loginPositionYaw",playerYaw);
-                            config.set("loginPositionPitch",playerPitch);
+                            double playerX = location.getX(),
+                                    playerY = location.getY(),
+                                    playerZ = location.getZ();
+                            float playerYaw = location.getYaw(),
+                                    playerPitch = location.getPitch();
+                            config.set("loginWorld", player.getWorld().getName());
+                            config.set("loginPositionX", playerX);
+                            config.set("loginPositionY", playerY);
+                            config.set("loginPositionZ", playerZ);
+                            config.set("loginPositionYaw", playerYaw);
+                            config.set("loginPositionPitch", playerPitch);
                             sender.sendMessage("Success to set your position to login position");
-                            sender.sendMessage("X:§b "+playerX+"§r ,Y:§b "+playerY+"§r ,Z:§b "+playerZ+"§r ,Yaw:§b "+playerYaw+"§r ,Pitch:§b "+playerPitch+".");
+                            sender.sendMessage("X:§b " + playerX + "§r ,Y:§b " + playerY + "§r ,Z:§b " + playerZ + "§r ,Yaw:§b " + playerYaw + "§r ,Pitch:§b " + playerPitch + ".");
 
                             plugin.saveConfig();
 
                             return true;
                         case "banworld":
-                            if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.banworld")){
+                            if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.banworld")) {
                                 sender.sendMessage("§cYou don't have permission to use this command.");
                                 return true;
                             }
@@ -77,7 +78,7 @@ public class CommandCustomLoginPosition {
 
                             return true;
                         case "reload":
-                            if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.reload")){
+                            if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.reload")) {
                                 sender.sendMessage("§cYou don't have permission to use this command.");
                                 return true;
                             }
@@ -88,53 +89,52 @@ public class CommandCustomLoginPosition {
                             return true;
                     }
                 case 2:
-                    switch (args[0]){
+                    switch (args[0]) {
                         case "banworld":
                             if (!(sender instanceof Player)) {
                                 sender.sendMessage("§cOnly player can use this command!");
                                 return true;
                             }
-                            if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.banworld")){
+                            if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.banworld")) {
                                 sender.sendMessage("§cYou don't have permission to use this command.");
                                 return true;
                             }
 
                             Player player = (Player) sender;
                             Configuration config = plugin.getConfig();
-                            List<String> banWorldList =  config.getStringList("banWorld");
+                            List<String> banWorldList = config.getStringList("banWorld");
 
-                            switch (args[1]){
+                            switch (args[1]) {
                                 case "add":
-                                    if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.banworld.add")){
+                                    if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.banworld.add")) {
                                         sender.sendMessage("§cYou don't have permission to use this command.");
                                         return true;
                                     }
 
                                     boolean isInBanWorldList = false;
-                                    for(String i : banWorldList){
-                                        if(i.equals(player.getWorld().getName())){
+                                    for (String i : banWorldList) {
+                                        if (i.equals(player.getWorld().getName())) {
                                             isInBanWorldList = true;
                                         }
                                     }
 
-                                    if(!isInBanWorldList){
+                                    if (!isInBanWorldList) {
                                         banWorldList.add(player.getWorld().getName());
-                                        config.set("banWorld",banWorldList);
+                                        config.set("banWorld", banWorldList);
                                         plugin.saveConfig();
-                                        sender.sendMessage("Added "+player.getWorld().getName()+" to ban world list.");
-                                    }
-                                    else{
-                                        sender.sendMessage(player.getWorld().getName()+" are already in ban world list.");
+                                        sender.sendMessage("Added " + player.getWorld().getName() + " to ban world list.");
+                                    } else {
+                                        sender.sendMessage(player.getWorld().getName() + " are already in ban world list.");
                                     }
 
                                     break;
                                 case "remove":
-                                    if(!CheckPermissions.hasPermissions(sender,"customloginposition.command.remove")){
+                                    if (!CheckPermissions.hasPermissions(sender, "customloginposition.command.remove")) {
                                         sender.sendMessage("§cYou don't have permission to use this command.");
                                         return true;
                                     }
                                     boolean isRemoved = banWorldList.remove(player.getWorld().getName());
-                                    config.set("banWorld",banWorldList);
+                                    config.set("banWorld", banWorldList);
                                     plugin.saveConfig();
                                     if (isRemoved) {
                                         sender.sendMessage("Removed " + player.getWorld().getName() + " in ban world list.");
