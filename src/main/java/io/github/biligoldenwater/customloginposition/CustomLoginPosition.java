5100350ba981b96b1b7d82dd64f6d9b006224d56
BiliGoldenWater <2439577268@qq.com>
2021-04-20 17:44:10 +0800

    let respawn teleport optional

--- src/main/java/io/github/biligoldenwater/customloginposition/CustomLoginPosition.java
+++ src/main/java/io/github/biligoldenwater/customloginposition/CustomLoginPosition.java
@@ -8,15 +8,18 @@ import io.github.biligoldenwater.customloginposition.listener.OnPlayerRespawnEve
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomLoginPosition extends JavaPlugin {
+    private static CustomLoginPosition instance;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+
         saveDefaultConfig();
 
-        getServer().getPluginManager().registerEvents(new OnPlayerJoinEvent(),this);
-        getServer().getPluginManager().registerEvents(new OnPlayerMoveEvent(),this);
-        getServer().getPluginManager().registerEvents(new OnPlayerRespawnEvent(),this);
+        getServer().getPluginManager().registerEvents(new OnPlayerJoinEvent(), this);
+        getServer().getPluginManager().registerEvents(new OnPlayerMoveEvent(), this);
+        getServer().getPluginManager().registerEvents(new OnPlayerRespawnEvent(), this);
 
         new CommandCustomLoginPosition(this);
         new TabCustomLoginPosition(this);
@@ -29,5 +32,9 @@ public final class CustomLoginPosition extends JavaPlugin {
         // Plugin shutdown logic
         saveConfig();
         getLogger().info("CustomLoginPosition Disabled");
+    }
+
+    public static CustomLoginPosition getInstance() {
+        return instance;
     }
 }
--- src/main/java/io/github/biligoldenwater/customloginposition/listener/OnPlayerRespawnEvent.java
+++ src/main/java/io/github/biligoldenwater/customloginposition/listener/OnPlayerRespawnEvent.java
@@ -1,17 +1,25 @@
 package io.github.biligoldenwater.customloginposition.listener;
 
+import io.github.biligoldenwater.customloginposition.CustomLoginPosition;
 import io.github.biligoldenwater.customloginposition.modules.GetLoginPosition;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.Plugin;
 
 public class OnPlayerRespawnEvent implements Listener {
     @EventHandler
-    public void onPlayerRespawnEvent(PlayerRespawnEvent e){
-        Location loginLocation = GetLoginPosition.getLoginPosition(Bukkit.getServer(),e.getPlayer(),Bukkit.getServer().getPluginManager().getPlugin("CustomLoginPosition").getConfig());
-        if(loginLocation!=null){
+    public void onPlayerRespawnEvent(PlayerRespawnEvent e) {
+        Plugin plugin = CustomLoginPosition.getInstance();
+        FileConfiguration config = plugin.getConfig();
+
+        if (!config.getBoolean("respawnAtLoginPosition")) return;
+
+        Location loginLocation = GetLoginPosition.getLoginPosition(Bukkit.getServer(), e.getPlayer(), config);
+        if (loginLocation != null) {
             e.setRespawnLocation(loginLocation);
         }
     }
